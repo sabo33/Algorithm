4e9cc6196ae92443d4be85150e0d3f8338e267de
kiyoun Nam <121239972+sabo33@users.noreply.github.com>
2024-06-03 16:20:01 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [] arr = new int[42];
+        int count =0;
+        for(int i=0; i<10; i++){
+          arr[Integer.parseInt(br.readLine())%42]++;
+        }
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]!=0)
+                count++;
+        }
+        System.out.println(count);
+
+
+    }
+}
