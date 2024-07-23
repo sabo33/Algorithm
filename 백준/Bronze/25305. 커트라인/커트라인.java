89d7b992fe50aca8daa849e8fce3adf003f3d261
kiyoun Nam <121239972+sabo33@users.noreply.github.com>
2024-07-23 12:19:09 +0900

    [Bronze II] Title: 커트라인, Time: 284 ms, Memory: 20716 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int k=sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=n-1; i>0; i--){
+            int temp=0;
+           for(int j=0; j<i; j++){
+               if(arr[j]>arr[j+1]){
+                   temp=arr[j];
+                   arr[j]=arr[j+1];
+                   arr[j+1]=temp;
+               }
+           }
+        }
+        System.out.println(arr[n-k]);
+    }
+}
