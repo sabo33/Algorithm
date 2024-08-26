cac3d217670b9866513941d51672ef8640967f86
kiyoun Nam <121239972+sabo33@users.noreply.github.com>
2024-08-26 15:53:37 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 636 ms, Memory: 93636 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String s = sc.nextLine();
-        int n = Integer.parseInt(sc.nextLine());
+        String s = br.readLine();
+        int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[26][s.length()+1];
 
         for(int i=0; i<s.length(); i++){
@@ -18,14 +21,14 @@ public class Main {
             arr[id][i+1]++;
         }
         for(int i=0; i<n; i++){
-            String alpha = sc.next();
-            int l = sc.nextInt();
-            int r = sc.nextInt();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String alpha = st.nextToken();
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
             int idx = alpha.charAt(0) - 'a';
             int result = arr[idx][r+1] - arr[idx][l];
             sb.append(result).append("\n");
         }
         System.out.println(sb);
-        sc.close();
     }
 }
